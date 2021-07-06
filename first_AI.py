78b9f66dfe5dcdf391ed5cf07a8beb45f3bed3c5
LDDesign <lucas03dewil@gmail.com>
2021-07-06 16:34:25 +0200

    Comments explenation

--- first_AI.py
+++ first_AI.py
@@ -1,4 +1,8 @@
 import numpy as np
 
 x_enter = np.array(([3,1.5],[2,1],[4,1.5],[3,1],[3.5,0.5],[5.5,1],[1,1],[4,1.5]),dtype=float)
+#entry of numbers
 y = np.array(([1], [0], [1],[0],[1],[0],[1],[0]), dtype=float)
+#1 is red and 0 is blue
+
+x_enter = x_enter/np.amax(x_enter,axis=0)#devide every entry with the highest entry
