3c8a64907e82a969cc1ff7cc2e13897f5fb1a6f1
LDDesign <lucas03dewil@gmail.com>
2021-07-06 16:52:55 +0200

    see comments

--- first_AI.py
+++ first_AI.py
@@ -21,3 +21,10 @@ class Neural_network(object):
         #our Weights
         self.W1 = np.random.randn(self.inputSize, self.hiddenSize) #2X3 Weight matrix between neurons entering and hidden
         self.W2 = np.random.randn(self.hiddenSize, self.outputSize) #3X1 Weight matrix between hidden neurons and output
+    #fonction of forward propagation
+    def forward(self,X):
+        self.z = np.dot(X,self.W1)# Matrix multiplication between input values and W1 weights
+        self.z2 = self.sigmoid(self.z) # Applying of the activation function (Sigmoid)
+        self.z3 = np.dot(self.z2, self.W2)# Matrix multiplication between hidden values and W2 weights
+        o = self.sigmoid(self.z)# Applying the activation function, and getting our final output value
+        return o
